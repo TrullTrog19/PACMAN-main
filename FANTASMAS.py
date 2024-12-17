a1ef0778d19b9bb0d1e638fec7e5d48f5272080c
ItzKivotPlay <ivangpb2006@gmail.com>
2024-12-17 23:14:08 +0100

    persecucion

--- FANTASMAS.py
+++ FANTASMAS.py
@@ -83,34 +83,6 @@ class Ghost1(Fantasma):
         new_x = self.ghost_x
         new_y = self.ghost_y
 
-
-        if self.huyendo:
-            if self.pacman.pacman_x < self.ghost_x:
-                new_x += velocidad
-            elif self.pacman.pacman_x > self.ghost_x:
-                new_x -= velocidad
-            
-            if self.pacman.pacman_y < self.ghost_y:
-                new_y += velocidad
-            elif self.pacman.pacman_y > self.ghost_y:
-                new_y -= velocidad
-        else:
-            if self.pacman.pacman_x < self.ghost_x:
-                new_x -= velocidad
-            elif self.pacman.pacman_x > self.ghost_x:
-                new_x += velocidad
-            
-            if self.pacman.pacman_y < self.ghost_y:
-                new_y -= velocidad
-            elif self.pacman.pacman_y > self.ghost_y:
-                new_y += velocidad
-        
-        if not self.mapa.hitbox_fantasma(new_x, new_y, self.ancho, self.alto):
-            self.ghost_x = new_x
-            self.ghost_y = new_y
-        else:
-            self.direccion = random.choice(["arriba", "abajo", "izquierda", "derecha"])
-
         if self.pacman_visto():
             if self.direccion == "izquierda" or self.direccion == "derecha":
                 if self.pacman.pacman_x < self.ghost_x:

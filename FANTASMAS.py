ac1017dbbc0e0628a057fd1081d61fe36471d105
TrullTrog19 <roblesserranoalberto@gmail.com>
2024-12-17 17:05:58 +0100

    fantasmas animacion

--- FANTASMAS.py
+++ FANTASMAS.py
@@ -1,36 +1,39 @@
 import pyxel
 
-# PARA QUE LOS FANTASMAS HEREDEN DE LA CLASE
 class Fantasma:
-    def __init__(self, x, y, img_x, img_y):
+    def __init__(self, x, y, frames):
         self.ghost_x = x
         self.ghost_y = y
-        self.img_x = img_x
-        self.img_y = img_y
+        self.frames = frames
+        self.current_frame = 0
 
     def update(self):
-        pass
+        self.current_frame = (self.current_frame + 1) % len(self.frames)
 
     def draw(self):
-        pyxel.blt(self.ghost_x, self.ghost_y, 2, self.img_x, self.img_y, 32, 32, 0)
+        img_x, img_y = self.frames[self.current_frame]
+        pyxel.blt(self.ghost_x, self.ghost_y, 2, img_x, img_y, 32, 32, 0)
 
 
 class Ghost1(Fantasma):
     def __init__(self):
-        super().__init__(120, 120, 0, 0)
+        frames = [(0, 0), (64, 0)]  # Provided frame coordinates
+        super().__init__(120, 120, frames)
 
 
 class Ghost2(Fantasma):
     def __init__(self):
-        super().__init__(220, 120, 32, 0)
+        frames = [(32, 0), (96, 0)]  # Provided frame coordinates
+        super().__init__(220, 120, frames)
 
 
 class Ghost3(Fantasma):
     def __init__(self):
-        super().__init__(220, 60, 0, 32)
+        frames = [(0, 32), (64, 32)]  # Provided frame coordinates
+        super().__init__(220, 60, frames)
 
 
 class Ghost4(Fantasma):
     def __init__(self):
-        super().__init__(10, 50, 32, 32)
-
+        frames = [(32, 32), (96, 32)]  # Provided frame coordinates
+        super().__init__(10, 50, frames)
